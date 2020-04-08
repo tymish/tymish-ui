c75bf1384d68522d89584a1a8583097c036454d4
Hubert Lin <linhub15@gmail.com>
2020-04-07 23:13:29 -0600

    implement ngrx effects

--- src/app/store/effects/employee.effect.ts
+++ src/app/store/effects/employee.effect.ts
@@ -1,27 +1,27 @@
 import { Injectable } from '@angular/core';
-import { createEffect, Actions, ofType } from '@ngrx/effects'
+import { map } from 'rxjs/operators';
 
+import { createEffect, Actions, ofType } from '@ngrx/effects'
 import * as EmployeeActions from '../actions/employee.action';
 
 import { EmployeesService } from '../../core/api/services/employees.service';
-import { mergeMap, map, catchError } from 'rxjs/operators';
-import { EMPTY } from 'rxjs';
+import { CreateEmployeeCommand } from '../../core/api/models/create-employee-command';
 
 @Injectable()
 export class EmployeeEffect {
+
+  addEmployee$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(EmployeeActions.addEmployee),
+      map((action) => {
+        this.employeeService.createEmployee({ body: action.employee as CreateEmployeeCommand });
+        return EmployeeActions.loadEmployees();
+      })
+    );
+  });
 
   constructor(
     private actions$: Actions,
     private employeeService: EmployeesService
   ) { }
-
-  addEmployee$ = createEffect(() => this.actions$.pipe(
-    ofType(''),
-    map(action => { }this.employeeService.createEmployee()
-      .pipe(
-        map(() => ({ type: EmployeeActions.LoadEmployees, payload: '' })),
-        catchError(() => EMPTY)
-      ))
-  )
-  );
 }
--- src/app/store/reducers/employee.reducer.ts
+++ src/app/store/reducers/employee.reducer.ts
@@ -1,25 +1,30 @@
-import { Action, createReducer, on } from '@ngrx/store';
-import { Employee } from '../../core/api/models/employee';
+import {createReducer, on, Action} from '@ngrx/store';
+import {Employee} from '../../core/api/models/employee';
 
 import * as EmployeeActions from '../actions/employee.action';
 
-const initialState: Employee = {
-
-}
+const initialState: Employee[] = [];
 
 const employeeReducer = createReducer(
   initialState,
-  on(
-    EmployeeActions.LoadEmployees,
-    EmployeeActions.AddEmployee,
-    EmployeeActions.LoadEmployees,
-    EmployeeActions.RemoveEmployee,
-    (state, { updatedValue }) => ({ ...state, prop: updatedValue })
-  ),
-  on(
-    //todo
+  on(EmployeeActions.loadEmployees, (state) => state),
+
+  on(EmployeeActions.addEmployee, (state, props) => {
+    state.push(props.employee);
+    return state;
+  }),
+
+  on(EmployeeActions.updateEmployee, (state, props) => {
+    const index = state.findIndex(
+      (employee) => employee.employeeNumber === props.employee.employeeNumber
+    );
+    return state.splice(index);
+  }),
+
+  on(EmployeeActions.removeEmployee, (state, props) =>
+    state.filter((employee) => employee.employeeNumber !== props.employeeNumber)
   )
-)
-export function reducer(state: Employee[] = [initialState], action: EmployeeActions) {
+);
+export function reducer(state: Employee[] | undefined, action: Action) {
   return employeeReducer(state, action);
 }
