43ed201a1c21899cc3d2a7f9bab3d646847ec621
Hubert Lin <linhub15@gmail.com>
2020-04-13 15:56:02 -0600

    setup auth0 environment details

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
-  api: "https://int.tymish-api.birdy.dev"
+  api: 'https://int.tymish-api.birdy.dev',
+  auth: {
+    domain: 'int-tymish.auth0.com',
+    client_id: 'DNKgvkmEa7LfkTmcdtYFWGw3Ejk5INT2'
+  }
 };
