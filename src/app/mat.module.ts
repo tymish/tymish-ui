b2918cefadb3fbe06ad5ae1611647b24e4d5fd5f
Hubert Lin <linhub15@gmail.com>
2020-04-17 22:01:06 -0600

    style filter side drawer

--- src/app/mat.module.ts
+++ src/app/mat.module.ts
@@ -18,6 +18,7 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatSelectModule} from '@angular/material/select';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatRadioModule} from '@angular/material/radio';
 
 const angularMaterial = [
   MatButtonModule,
@@ -38,7 +39,8 @@ const angularMaterial = [
   MatProgressBarModule,
   MatSelectModule,
   MatMenuModule,
-  MatSnackBarModule
+  MatSnackBarModule,
+  MatRadioModule
 ];
 
 @NgModule({
