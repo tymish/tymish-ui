a22b6c99d2e3e8c821b56fd57112bde58ad0cc7d
ianrbaguio <ianrey_2004@yahoo.com>
2020-04-19 22:08:32 -0600

    Added Employee Validations

--- src/app/employees/add-employee/add-employee.component.ts
+++ src/app/employees/add-employee/add-employee.component.ts
@@ -1,10 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {FormGroup, FormControl} from '@angular/forms';
-import {CreateEmployeeCommand} from 'src/app/core/api/models';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CreateEmployeeCommand } from 'src/app/core/api/models';
 
-import {addEmployee} from '../store/employee.action';
-import {Store} from '@ngrx/store';
-import {AppState} from 'src/app/core/app.state';
+import { addEmployee } from '../store/employee.action';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/core/app.state';
 
 @Component({
   selector: 'app-add-employee',
@@ -12,24 +12,27 @@ import {AppState} from 'src/app/core/app.state';
   styleUrls: ['./add-employee.component.scss']
 })
 export class AddEmployeeComponent implements OnInit {
+  // email regex reference: http://regexlib.com/Search.aspx?k=email&AspxAutoDetectCookieSupport=1
   public form = new FormGroup({
-    givenName: new FormControl(''),
-    familyName: new FormControl(''),
-    email: new FormControl(''),
-    hourlyPay: new FormControl('')
+    givenName: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+    familyName: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+    email: new FormControl('', [Validators.required, Validators.pattern('^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$')]),
+    hourlyPay: new FormControl('', [Validators.required, Validators.min(0), Validators.max(100)])
   });
 
-  constructor(private store: Store<AppState>) {}
+  constructor(private store: Store<AppState>) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   addEmployee(): void {
-    const employee: CreateEmployeeCommand = {
-      givenName: this.form.get('givenName').value,
-      familyName: this.form.get('familyName').value,
-      email: this.form.get('email').value,
-      hourlyPay: +this.form.get('hourlyPay').value
-    };
-    this.store.dispatch(addEmployee({employee: employee}));
+    if (this.form.valid) {
+      const employee: CreateEmployeeCommand = {
+        givenName: this.form.get('givenName').value,
+        familyName: this.form.get('familyName').value,
+        email: this.form.get('email').value,
+        hourlyPay: +this.form.get('hourlyPay').value
+      };
+      this.store.dispatch(addEmployee({ employee: employee }));
+    }
   }
 }
