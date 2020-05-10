46885cbaf79aca39aec33f6ec6a3569e6073163f
Hubert Lin <linhub15@gmail.com>
2020-05-09 18:37:06 -0600

    remove submit-invoice, moved to the ng-vendor app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {SubmitInvoiceComponent} from './invoices/submit-invoice/submit-invoice.component';
 import {LoginComponent} from './core/auth/login.component';
 import {LoggedOutComponent} from './core/auth/logged-out.component';
 import {AuthGuard} from './core/auth/auth.guard';
@@ -8,8 +7,6 @@ import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'submit-invoice', component: SubmitInvoiceComponent},
-  {path: 'submit-invoice/:id', component: SubmitInvoiceComponent},
   {path: 'login', component: LoginComponent},
   {path: 'logged-out', component: LoggedOutComponent},
 
