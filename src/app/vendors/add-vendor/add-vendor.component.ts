8a45ecce7a87edd19e3a6882edeeb6bef71804ca
Hubert Lin <linhub15@gmail.com>
2020-08-08 14:57:19 -0600

    implement UC2.0 add vendor

--- src/app/vendors/add-vendor/add-vendor.component.ts
+++ src/app/vendors/add-vendor/add-vendor.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {VendorsService} from 'src/app/core/api/services';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-add-vendors',
@@ -6,7 +8,12 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./add-vendor.component.scss']
 })
 export class AddVendorComponent implements OnInit {
-  constructor() {}
+  constructor(private vendors: VendorsService, private router: Router) {}
 
   ngOnInit(): void {}
+
+  addVendor(email: string) {
+    this.vendors.addVendor({body: {email: email}}).subscribe();
+    this.router.navigate(['vendors']);
+  }
 }
