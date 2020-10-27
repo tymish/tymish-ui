04a73cc43d37625b04166b4149d9f95d92312bba
Hubert Lin <linhub15@gmail.com>
2020-10-26 23:46:02 -0600

    add firstname lastname to add vendor

--- src/app/vendors/add-vendor/add-vendor.component.ts
+++ src/app/vendors/add-vendor/add-vendor.component.ts
@@ -1,6 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {VendorsService} from 'src/app/core/api/services';
 import {Router} from '@angular/router';
+import {FormControl, FormGroup} from '@angular/forms';
+import {IFormGroup} from '@rxweb/types';
+
+interface AddVendorForm {
+  email: string;
+  givenName: string;
+  familyName: string;
+}
 
 @Component({
   selector: 'app-add-vendors',
@@ -10,10 +18,36 @@ import {Router} from '@angular/router';
 export class AddVendorComponent implements OnInit {
   constructor(private vendors: VendorsService, private router: Router) {}
 
+  form = new FormGroup({
+    email: new FormControl(''),
+    givenName: new FormControl(''),
+    familyName: new FormControl('')
+  }) as IFormGroup<AddVendorForm>;
+
+  get email() {
+    return this.form.controls.email;
+  }
+
+  get givenName() {
+    return this.form.controls.givenName;
+  }
+
+  get familyName() {
+    return this.form.controls.familyName;
+  }
+
   ngOnInit(): void {}
 
-  addVendor(email: string) {
-    this.vendors.addVendor({body: {email: email}}).subscribe();
+  addVendor() {
+    this.vendors
+      .addVendor({
+        body: {
+          email: this.email.value,
+          givenName: this.givenName.value,
+          familyName: this.familyName.value
+        }
+      })
+      .subscribe();
     this.router.navigate(['vendors']);
   }
 }
