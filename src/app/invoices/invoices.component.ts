e34f6e8801f9f139af956242f84a9581a04dcdaa
Hubert Lin <linhub15@gmail.com>
2020-11-05 18:03:16 -0700

    pay invoice

--- src/app/invoices/invoices.component.ts
+++ src/app/invoices/invoices.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 import {InvoiceDto, InvoiceStatus} from '../core/api/models';
 import {InvoicesService} from '../core/api/services';
 
@@ -15,9 +16,11 @@ export class InvoicesComponent implements OnInit {
   paidInvoices$: Observable<InvoiceDto[]>;
 
   ngOnInit(): void {
-    this.submittedInvoices$ = this.service.listInvoices({
-      status: InvoiceStatus.Submitted
-    });
+    this.submittedInvoices$ = this.service
+      .listInvoices({
+        status: InvoiceStatus.Submitted
+      })
+      .pipe(map((invoices) => invoices.filter((invoice) => !invoice.paid)));
 
     this.paidInvoices$ = this.service.listInvoices({
       status: InvoiceStatus.Paid
--- src/app/invoices/pay-invoice/pay-invoice.component.ts
+++ src/app/invoices/pay-invoice/pay-invoice.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import { Observable } from 'rxjs';
-import { InvoiceDto } from 'src/app/core/api/models';
-import { InvoicesService } from 'src/app/core/api/services';
+import {Observable} from 'rxjs';
+import {InvoiceDto} from 'src/app/core/api/models';
+import {InvoicesService} from 'src/app/core/api/services';
 
 @Component({
   selector: 'app-pay-invoice',
@@ -13,12 +13,25 @@ export class PayInvoiceComponent implements OnInit {
   constructor(
     private readonly route: ActivatedRoute,
     private readonly service: InvoicesService
-    ) {}
+  ) {}
 
+  invoiceId: string;
   invoice$: Observable<InvoiceDto>;
 
   ngOnInit(): void {
-    const invoiceId = this.route.snapshot.params['id'];
-    this.invoice$ = this.service.getInvoiceById({id: invoiceId});
+    this.invoiceId = this.route.snapshot.params['id'];
+    this.invoice$ = this.service.getInvoiceById({id: this.invoiceId});
+  }
+
+  payInvoice(amount: number, reference: string) {
+    this.service
+      .payInvoice({
+        body: {
+          invoiceId: this.invoiceId,
+          paymentAmount: amount,
+          paymentReference: reference
+        }
+      })
+      .subscribe();
   }
 }
