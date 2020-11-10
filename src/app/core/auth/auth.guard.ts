52422f6a9523ff2e6f606bdb93159ac85b9f5603
Hubert Lin <linhub15@gmail.com>
2020-11-09 19:50:30 -0700

    redirect to login if navigate to `/`

--- src/app/core/auth/auth.guard.ts
+++ src/app/core/auth/auth.guard.ts
@@ -3,7 +3,8 @@ import {
   CanActivate,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
-  UrlTree
+  UrlTree,
+  Router
 } from '@angular/router';
 import {Observable} from 'rxjs';
 import {tap} from 'rxjs/operators';
@@ -13,12 +14,16 @@ import {AuthService} from './auth.service';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private readonly auth: AuthService) {}
+  constructor(
+    private readonly auth: AuthService,
+    private readonly router: Router
+  ) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean | UrlTree> | boolean {
+    if (!this.auth.loggedIn) this.router.navigate(['login']);
     return this.auth.loggedIn;
   }
 }
