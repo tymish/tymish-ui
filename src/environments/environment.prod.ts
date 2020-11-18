c7905246746ad4411ee5bee28e371eb46e44da37
Hubert Lin <linhub15@gmail.com>
2020-11-17 17:36:04 -0700

    update environment files to point to api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production: true,
-  api: 'https://int.tymish-api.birdy.dev',
-  auth: {
-    domain: 'int-tymish.auth0.com',
-    client_id: 'DNKgvkmEa7LfkTmcdtYFWGw3Ejk5INT2'
-  }
+  api: 'https://int.api.tymish.app'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production: false,
-  api: 'http://127.0.0.1:5000',
-  auth: {
-    domain: 'dev-tymish.auth0.com',
-    client_id: 'QMM1boy8gcni78zrRvBWkHpkUoqtC96r'
-  }
+  api: 'http://127.0.0.1:5000'
 };
